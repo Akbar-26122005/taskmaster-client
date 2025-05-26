518f035bd31f36c3c7d4afe938929fbcfc03179d
Akbar-26122005 <nurmatovakbar59@gmail.com>
2025-05-26 13:15:44 +0500

    Добавлен модуль для генерации токена

---
+++ src/components/secureToken.js
@@ -0,0 +1,11 @@
+import SHA256 from 'crypto-js/SHA256';
+
+export function sendSecureRequest(userId) {
+    const timestamp = Date.now();
+    const nonce = Math.random().toString(36).substring(2, 10);
+    const raw = `${userId}:${process.env.REACT_APP_KEY}:${nonce}`;
+    const token = SHA256(raw).toString();
+
+    const tokenWithTimestamp = `${timestamp}-${token}-${nonce}`;
+    return tokenWithTimestamp;
+}
